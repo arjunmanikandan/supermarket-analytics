e746b95a564033771be3c2e09fe3ff7a98d855f2
arjunmanikandan <arjunmanikandan319@gmail.com>
2024-06-25 16:09:05 +0530

    Calculated taxes per product state wise

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Calculated Price Inclusive of taxes for each product based on the given state
+#Calculated Price Inclusive of taxes for each product based on the state list
 import pandas as pd
 import sys,json
 
@@ -11,25 +11,28 @@ def read_csv(csv_path):
     df = pd.read_csv(csv_path)
     return df
 
-def display_fare_details(fare_details_df):
-    print(fare_details_df)
+def display_fare_details(menu_df_list):
+    menu_df_combined = pd.concat(menu_df_list).reset_index(drop=True)
+    print(menu_df_combined)
 
 def calc_cost_details(row,super_market_df,menu):
     min_rate = super_market_df[super_market_df["Product"] == row["Product"]]["min"].values[0]
     menu.at[row.name,"MinimumRate"] =  min_rate
     return row["Quantity"] * min_rate
 
-def calc_grocery_state_tax(state_tax_df,state_name):
-    tax = state_tax_df.query("State == @state_name")["Tax(%)"].iat[0]
+def calc_grocery_state_tax(state_tax_df,state):
+    tax = state_tax_df.query("State == @state")["Tax(%)"].iat[0]
     return tax
 
-def calc_tax_details(price,tax_data):
-    return price*tax_data + price
+def calc_tax_details(row,tax_data,menu,state):
+    menu.at[row.name,"Tax(%)"] = tax_data
+    menu.at[row.name,"State"] = state
+    return row["Price"]*tax_data/100 + row["Price"]
 
-def get_cost_details(supermarket_products, menu,grocery_tax,tax_df):
+def get_cost_details(supermarket_products, menu,grocery_tax,state):
     supermarket_products["min"] = supermarket_products[["SevenEleven($)","Walmart($)","Tesco($)"]].min(axis=1)
     menu["Price"] = menu.apply(calc_cost_details,args=(supermarket_products,menu),axis=1)
-    menu["Price(IncTax)"] = menu["Price"].apply(calc_tax_details,args=(grocery_tax/100,))
+    menu["Price(IncTax)"] = menu.apply(calc_tax_details,args=(grocery_tax,menu,state),axis=1)
     return menu
 
 def extract_csv_contents(config_file):
@@ -42,15 +45,17 @@ def extract_csv_contents(config_file):
         "tax_data":state_taxes
     }
     return csv_data
-
-#User Input: python3 main.py config.json Idaho
+#User Input: python3 main.py config.json state_list
 def main():
-    config_file = read_json(sys.argv[1])
-    state_name = sys.argv[2]
+    menu_df_list = []
+    config_file  = read_json("config.json")
     csv_data = extract_csv_contents(config_file)
-    grocery_tax = calc_grocery_state_tax(csv_data["tax_data"],state_name)
-    fare_details_df = get_cost_details(csv_data["supermarket_data"],
-    csv_data["menu_data"],grocery_tax,csv_data["tax_data"])
-    display_fare_details(fare_details_df)
+    states = csv_data["tax_data"]["State"].unique()
+    for state in  states:
+        grocery_tax = calc_grocery_state_tax(csv_data["tax_data"],state)
+        menu_df = get_cost_details(csv_data["supermarket_data"],
+        csv_data["menu_data"],grocery_tax,state)
+        menu_df_list.append(menu_df.copy())
+    display_fare_details(menu_df_list)
 
 main()
