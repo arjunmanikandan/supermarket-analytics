fbf56f0932c547e0c15b140e064529bb9d556336
arjunmanikandan <arjunmanikandan319@gmail.com>
2024-07-02 17:38:26 +0530

    Introduced discounts and Improvised Functions

--- main.py
+++ main.py
@@ -27,42 +27,63 @@ def calc_cost_details(row):
 def calc_transportation_charges(row,charges):
     charges_index = charges[charges["State"] == row["State"]].index
     transport_charges = charges.at[charges_index[0],"TransportationCharges($)"]
-    transportation_charges = transport_charges if row["Quantity"] <=1000 else math.ceil(row["Quantity"]/1000) * transport_charges
+    transportation_charges = math.ceil(row["Quantity"]/1000) * transport_charges
     columns_dict = {
-        "Rate($)":transportation_charges
+        "PriceIncTransportation($)":transportation_charges
     }
     return pd.Series(columns_dict)
 
-#The transportation charges listed in the CSV file are per 1000 units
-def get_cost_details(supermarket_products,menu,taxes,charges):
-    products_menu_df = pd.merge(supermarket_products,menu,on=["Product"],how="outer")
-    menu_taxes_df = pd.merge(products_menu_df,taxes,on=["Product","State"],how="left").fillna(0)
+def calc_discount(row,discount):
+    product_index = discount[discount["Product"]==row["Product"]].index
+    minimum_quantity = discount.at[product_index[0], "MinimumQuantity"]
+    discount_percentage = discount.at[product_index[0], "Discount(%)"]
+    discount_value = row["Cost($)"]  if row["Quantity"] < minimum_quantity else row["Cost($)"] - discount_percentage/100 * row["Cost($)"] 
+    columns_dict = {
+        "Discount(%)":discount_percentage,
+        "DiscountedPrice($)":discount_value
+    }
+    return pd.Series(columns_dict)
+
+def calc_total_cost(row):
+    columns_dict = {
+        "TotalCost": row["DiscountedPrice($)"]+row["PriceIncTransportation($)"]
+    }
+    return pd.Series(columns_dict)
+
+#Reduced the number of parameters
+def get_cost_details(csv_data):
+    products_menu_df = pd.merge(csv_data["products_data"],csv_data["menu_data"],on=["Product"],how="outer")
+    menu_taxes_df = pd.merge(products_menu_df,csv_data["tax_data"],on=["Product","State"],how="left").fillna(0)
     menu_taxes_df["Cost($)"] = menu_taxes_df.apply(calc_cost_details,axis=1)
-    menu_dfs = menu.apply(extract_cheapest_prod_price,args=(menu_taxes_df,),axis=1)
+    menu_dfs = csv_data["menu_data"].apply(extract_cheapest_prod_price,args=(menu_taxes_df,),axis=1)
     least_prod_cost_df = list(map(lambda df : df,menu_dfs))
     least_prod_cost_df = pd.concat(least_prod_cost_df,ignore_index=True)
-    menu_charges_df = pd.merge(least_prod_cost_df,charges,on=["State"],how="left")
-    menu_charges_df["Rate($)"] = menu_charges_df.apply(calc_transportation_charges,args=(charges,),axis=1)
+    menu_charges_df = pd.merge(least_prod_cost_df,csv_data["transportation_data"],on=["State"],how="left")
+    menu_charges_df["PriceIncTransportation($)"] = menu_charges_df.apply(calc_transportation_charges,args=(csv_data["transportation_data"],),axis=1)
+    menu_charges_df[["Discount(%)","DiscountedPrice($)"]] = menu_charges_df.apply(calc_discount,args=(csv_data["discount_data"],),axis=1)
+    menu_charges_df["TotalCost($)"] = menu_charges_df.apply(calc_total_cost,axis=1)
     return menu_charges_df
-    
+
 def extract_csv_contents(config):
     supermarket_products = read_csv(config["supermarket_products_path"])
     menu = read_csv(config["menu_path"])
     state_taxes = read_csv(config["state_taxes_path"])
     transportation_charges = read_csv(config["transportation_charges_path"])
+    discount_percentage = read_csv(config["bulk_discount_path"])
     csv_data = {
         "products_data":supermarket_products,
         "menu_data":menu,
         "tax_data":state_taxes,
-         "transportation_data":transportation_charges
+         "transportation_data":transportation_charges,
+         "discount_data":discount_percentage
+
     }
     return csv_data
 
 def main():
     config = read_json(sys.argv[1])
     csv_data = extract_csv_contents(config)
-    least_prod_cost_df = get_cost_details(csv_data["products_data"], 
-    csv_data["menu_data"],csv_data["tax_data"],csv_data["transportation_data"])
+    least_prod_cost_df = get_cost_details(csv_data)
     display_df(least_prod_cost_df)
 
 main()
